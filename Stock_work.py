82571eb7ef5921083956be8cd547cb2f4c4818ed
jallal89 <jallal.isa89@gmail.com>
2024-08-06 16:13:34 -0500

    Create Stock_work.py

---
+++ Stock_work.py
@@ -0,0 +1,3 @@
+# This project is used to analyze stock/finance data in Skills Network
+# Various tools and libraries in Pythin are used to work with this financial data
+# Some of these include: Yfinance method, web scraping, plot and more 
